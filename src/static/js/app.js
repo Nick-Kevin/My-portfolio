9a3e069bef7b13c5051036f9263308c4c6776fe4
Nick-Kevin <nicblou23@gmail.com>
2023-10-30 17:35:36 +0000

    update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -41,6 +41,8 @@ const router = async () => {
     console.log(match);
 }
 
+document.addEventListener('popstate', router);
+
 document.addEventListener('DOMContentLoaded', () => {
     document.body.addEventListener('click', e => {
         if(e.target.matches('[data-link]')) {
