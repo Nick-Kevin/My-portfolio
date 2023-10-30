eb52d4dd566616b04ed2c27c41c1a6328db50e92
Nick-Kevin <nicblou23@gmail.com>
2023-10-30 19:07:37 +0000

    last update

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -51,10 +51,8 @@ document.addEventListener('popstate', router);
 
 document.addEventListener('DOMContentLoaded', () => {
     document.body.addEventListener('click', e => {
-        if(e.target.matches('[data-link]')) {
             e.preventDefault();
             navigateTo(e.target.href);
-        }
     })
 
     router();
