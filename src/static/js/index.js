f11b2657feb2c6237f666c5a96aa729e7afb0ce6
Nick-Kevin <nicblou23@gmail.com>
2023-11-17 09:54:07 +0000

    udpate index.js

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -4,8 +4,11 @@ import Work from './pages/Work.js';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
 import { Ball } from './canvas/balls.js';
+import nodeGardenMass from './canvas/node-garden-mass.js'
 
 var windowHeight = window.innerHeight;
+
+export const portraitMode = window.matchMedia("(orientation: portrait)");
 
 const routes = [
     {
@@ -203,8 +206,6 @@ const router = async () => {
     const view = new match.view();
     document.querySelector("#home").innerHTML = await view.getHtml();
 
-    let portraitMode = window.matchMedia("(orientation: portrait)");
-
     switch(match.path) {
 
         case "/":
@@ -271,179 +272,9 @@ const router = async () => {
             break;
 
         case "/about":
-            var nodeGardenMassCanvas = document.getElementById('node-garden-mass');
+            nodeGardenMass();
 
-            nodeGardenMassCanvas.width = window.innerWidth;
-            nodeGardenMassCanvas.height = window.innerHeight;            
-
-            var context = nodeGardenMassCanvas.getContext('2d'),
-                particles = [],
-                numParticles = portraitMode.matches ? 35 : 50,
-                minDist = 100,
-                springAmount = 0.0001;
-           
-            var width = nodeGardenMassCanvas.width;
-            var height = nodeGardenMassCanvas.height;
-           
-            for(var i=0; i<numParticles; i++){
-                var color = Math.random()*(0xffffff);
-                var size = portraitMode.matches ? Math.random()*5 + 2 : Math.random()*5 + 5;
-                var ball = new Ball(size, color);
-               
-                    ball.x = Math.random()*width;
-                    ball.y = Math.random()*height;
-                    ball.vx = Math.random()*2 - 1;
-                    ball.vy = Math.random()*2 - 1;
-                    ball.mass = size;
-               
-                particles.push(ball);
-            }
-           
-            function gravaite(ballA, ballB){
-                var dx = ballB.x - ballA.x;
-                var dy = ballB.y - ballA.y;
-                var dist = Math.sqrt(dx*dx + dy*dy);
-               
-                if (dist < minDist) {                   
-                    context.save();
-                    context.strokeStyle = "rgba(255,255,255,.3)";
-                    context.beginPath();
-                    context.moveTo(ballA.x, ballA.y);
-                    context.lineTo(ballB.x, ballB.y);
-                    context.closePath();
-                    context.stroke();
-                    context.restore();               
-                
-                    var ax = dx * springAmount,
-                        ay = dy * springAmount;
-                    ballA.vx += ax / ballA.mass;
-                    ballA.vy += ay / ballA.mass;
-                    ballB.vx -= ax / ballB.mass;
-                    ballB.vy -= ay / ballB.mass;
-                }
-           }
-
-            function move(ballA, i){
-                ballA.x += ballA.vx;
-                ballA.y += ballA.vy;
-
-                if (ballA.x > nodeGardenMassCanvas.width) {
-                    ballA.x = 0;
-                } else if (ballA.x < 0) {
-                    ballA.x = nodeGardenMassCanvas.width;
-                }
-
-                if (ballA.y > nodeGardenMassCanvas.height) {
-                    ballA.y = 0;
-                } else if (ballA.y < 0) {
-                    ballA.y = nodeGardenMassCanvas.height;
-                }
-               
-                for(var ballB, j=i+1; j<numParticles; j++){
-                    ballB = particles[j];
-                    gravaite(ballA, ballB);
-                }
-            }
-           
-            function draw(ball){
-                ball.draw(context);
-            }
-           
-            (function drawFrmae(){
-                window.requestAnimationFrame(drawFrmae, nodeGardenMassCanvas);
-                context.clearRect(0, 0, width, height);
-               
-                particles.forEach(move);
-                particles.forEach(draw);
-            }());
-
-            window.addEventListener("resize", () => {
-                var nodeGardenMassCanvas = document.getElementById('node-garden-mass');
-
-            nodeGardenMassCanvas.width = window.innerWidth;
-            nodeGardenMassCanvas.height = window.innerHeight;            
-
-            var context = nodeGardenMassCanvas.getContext('2d'),
-                particles = [],
-                numParticles = portraitMode.matches ? 35 : 50,
-                minDist = 100,
-                springAmount = 0.0001;
-           
-            var width = nodeGardenMassCanvas.width;
-            var height = nodeGardenMassCanvas.height;
-           
-            for(var i=0; i<numParticles; i++){
-                var color = Math.random()*(0xffffff);
-                var size = portraitMode.matches ? Math.random()*5 + 2 : Math.random()*5 + 5;
-                var ball = new Ball(size, color);
-               
-                    ball.x = Math.random()*width;
-                    ball.y = Math.random()*height;
-                    ball.vx = Math.random()*2 - 1;
-                    ball.vy = Math.random()*2 - 1;
-                    ball.mass = size;
-               
-                particles.push(ball);
-            }
-           
-            function gravaite(ballA, ballB){
-                var dx = ballB.x - ballA.x;
-                var dy = ballB.y - ballA.y;
-                var dist = Math.sqrt(dx*dx + dy*dy);
-               
-                if (dist < minDist) {                   
-                    context.save();
-                    context.strokeStyle = "rgba(255,255,255,.3)";
-                    context.beginPath();
-                    context.moveTo(ballA.x, ballA.y);
-                    context.lineTo(ballB.x, ballB.y);
-                    context.closePath();
-                    context.stroke();
-                    context.restore();               
-                
-                    var ax = dx * springAmount,
-                        ay = dy * springAmount;
-                    ballA.vx += ax / ballA.mass;
-                    ballA.vy += ay / ballA.mass;
-                    ballB.vx -= ax / ballB.mass;
-                    ballB.vy -= ay / ballB.mass;
-                }
-           }
-
-            function move(ballA, i){
-                ballA.x += ballA.vx;
-                ballA.y += ballA.vy;
-
-                if (ballA.x > nodeGardenMassCanvas.width) {
-                    ballA.x = 0;
-                } else if (ballA.x < 0) {
-                    ballA.x = nodeGardenMassCanvas.width;
-                }
-
-                if (ballA.y > nodeGardenMassCanvas.height) {
-                    ballA.y = 0;
-                } else if (ballA.y < 0) {
-                    ballA.y = nodeGardenMassCanvas.height;
-                }
-               
-                for(var ballB, j=i+1; j<numParticles; j++){
-                    ballB = particles[j];
-                    gravaite(ballA, ballB);
-                }
-            }
-           
-            function draw(ball){
-                ball.draw(context);
-            }
-           
-            (function drawFrmae(){
-                window.requestAnimationFrame(drawFrmae, nodeGardenMassCanvas);
-                context.clearRect(0, 0, width, height);
-               
-                particles.forEach(move);
-                particles.forEach(draw);
-            }());  
-            })
+            window.addEventListener("resize", nodeGardenMass);
        break;
     }
 }
