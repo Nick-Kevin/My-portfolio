8eff69df669f8f09d00f769692099ffa18bbdb5f
Nick-Kevin <nicblou23@gmail.com>
2023-11-17 11:14:58 +0000

    udpate node-garden-mass.js

--- src/static/js/canvas/node-garden-mass.js
+++ src/static/js/canvas/node-garden-mass.js
@@ -1,101 +1,7 @@
 import { portraitMode } from '../index.js';
 import { Ball } from './balls.js';
 
-/*export default function nodeGardenMass() {
-	var nodeGardenMassCanvas = document.getElementById('node-garden-mass');
-
-            nodeGardenMassCanvas.width = window.innerWidth;
-            nodeGardenMassCanvas.height = window.innerHeight;            
-
-            var context = nodeGardenMassCanvas.getContext('2d'),
-                particles = [],
-                numParticles = portraitMode.matches ? 35 : 50,
-                minDist = 100,
-                springAmount = 0.0001;
-           
-            var width = nodeGardenMassCanvas.width;
-            var height = nodeGardenMassCanvas.height;
-           
-            for(var i=0; i<numParticles; i++){
-                var color = Math.random()*(0xffffff);
-                var size = portraitMode.matches ? Math.random()*5 + 2 : Math.random()*5 + 5;
-                var ball = new Ball(size, color);
-               
-                    ball.x = Math.random()*width;
-                    ball.y = Math.random()*height;
-                    ball.vx = Math.random()*2 - 1;
-                    ball.vy = Math.random()*2 - 1;
-                    ball.mass = size;
-               
-                particles.push(ball);
-            }
-           
-            function gravaite(ballA, ballB){
-                var dx = ballB.x - ballA.x;
-                var dy = ballB.y - ballA.y;
-                var dist = Math.sqrt(dx*dx + dy*dy);
-               
-                if (dist < minDist) {                   
-                    context.save();
-                    context.strokeStyle = "rgba(255,255,255,.3)";
-                    context.beginPath();
-                    context.moveTo(ballA.x, ballA.y);
-                    context.lineTo(ballB.x, ballB.y);
-                    context.closePath();
-                    context.stroke();
-                    context.restore();               
-                
-                    var ax = dx * springAmount,
-                        ay = dy * springAmount;
-                    ballA.vx += ax / ballA.mass;
-                    ballA.vy += ay / ballA.mass;
-                    ballB.vx -= ax / ballB.mass;
-                    ballB.vy -= ay / ballB.mass;
-                }
-           }
-
-            function move(ballA, i){
-                ballA.x += ballA.vx;
-                ballA.y += ballA.vy;
-
-                if (ballA.x > nodeGardenMassCanvas.width) {
-                    ballA.x = 0;
-                } else if (ballA.x < 0) {
-                    ballA.x = nodeGardenMassCanvas.width;
-                }
-
-                if (ballA.y > nodeGardenMassCanvas.height) {
-                    ballA.y = 0;
-                } else if (ballA.y < 0) {
-                    ballA.y = nodeGardenMassCanvas.height;
-                }
-               
-                for(var ballB, j=i+1; j<numParticles; j++){
-                    ballB = particles[j];
-                    gravaite(ballA, ballB);
-                }
-            }
-           
-            function draw(ball){
-                ball.draw(context);
-            }
-           
-            (function drawFrmae(){
-                window.requestAnimationFrame(drawFrmae, nodeGardenMassCanvas);
-                context.clearRect(0, 0, width, height);
-               
-                particles.forEach(move);
-                particles.forEach(draw);
-            }());
-}*/
-
 export default class nodeGardenMass {
-	/*message = "hi! I'm training to tunr it to poo";
-
-	showMessage() {
-		alert(this.message);
-	}*/
-
 	nodeGardenMassCanvas = document.getElementById('node-garden-mass');
 
 	context = this.nodeGardenMassCanvas.getContext('2d');
@@ -126,69 +32,61 @@ export default class nodeGardenMass {
         }
 	}
 
-	gravaite(ballA, ballB){
-                var dx = ballB.x - ballA.x;
-                var dy = ballB.y - ballA.y;
-                var dist = Math.sqrt(dx*dx + dy*dy);
-               
-                if (dist < this.minDist) {                   
-                    this.context.save();
-                    this.context.strokeStyle = "rgba(255,255,255,.3)";
-                    this.context.beginPath();
-                    this.context.moveTo(ballA.x, ballA.y);
-                    this.context.lineTo(ballB.x, ballB.y);
-                    this.context.closePath();
-                    this.context.stroke();
-                    this.context.restore();               
-                
-                    var ax = dx * this.springAmount,
-                        ay = dy * this.springAmount;
-                    ballA.vx += ax / ballA.mass;
-                    ballA.vy += ay / ballA.mass;
-                    ballB.vx -= ax / ballB.mass;
-                    ballB.vy -= ay / ballB.mass;
-                }
-           }
+	gravaite(ballA, ballB) {
+        var dx = ballB.x - ballA.x;
+        var dy = ballB.y - ballA.y;
+        var dist = Math.sqrt(dx*dx + dy*dy);
+       
+        if (dist < this.minDist) {                   
+            this.context.save();
+            this.context.strokeStyle = "rgba(255,255,255,.3)";
+            this.context.beginPath();
+            this.context.moveTo(ballA.x, ballA.y);
+            this.context.lineTo(ballB.x, ballB.y);
+            this.context.closePath();
+            this.context.stroke();
+            this.context.restore();               
+        
+            var ax = dx * this.springAmount,
+                ay = dy * this.springAmount;
+            ballA.vx += ax / ballA.mass;
+            ballA.vy += ay / ballA.mass;
+            ballB.vx -= ax / ballB.mass;
+            ballB.vy -= ay / ballB.mass;
+        }
+   	}
 
-    move(ballA, i){
-                ballA.x += ballA.vx;
-                ballA.y += ballA.vy;
+    move(ballA, i) {
+        ballA.x += ballA.vx;
+        ballA.y += ballA.vy;
 
-                if (ballA.x > this.nodeGardenMassCanvas.width) {
-                    ballA.x = 0;
-                } else if (ballA.x < 0) {
-                    ballA.x = this.nodeGardenMassCanvas.width;
-                }
-
-                if (ballA.y > this.nodeGardenMassCanvas.height) {
-                    ballA.y = 0;
-                } else if (ballA.y < 0) {
-                    ballA.y = this.nodeGardenMassCanvas.height;
-                }
-               
-                for(var ballB, j=i+1; j<this.particlesLength; j++){
-                    ballB = this.particles[j];
-                    this.gravaite(ballA, ballB);
-                }
-            }
-
-    draw(ball){
-                ball.draw(this.context);
-            }
-
-    drawFrmae(){
-            window.requestAnimationFrame(this.drawFrmae.bind(this), this.nodeGardenMassCanvas);
-            this.context.clearRect(0, 0, this.width, this.height);
-           
-            this.particles.forEach(this.move.bind(this));
-            this.particles.forEach(this.draw.bind(this));
+        if (ballA.x > this.nodeGardenMassCanvas.width) {
+            ballA.x = 0;
+        } else if (ballA.x < 0) {
+            ballA.x = this.nodeGardenMassCanvas.width;
         }
 
-    showMessage() {
-    	console.log(this.particles);
+        if (ballA.y > this.nodeGardenMassCanvas.height) {
+            ballA.y = 0;
+        } else if (ballA.y < 0) {
+            ballA.y = this.nodeGardenMassCanvas.height;
+        }
+       
+        for(var ballB, j=i+1; j<this.particlesLength; j++){
+            ballB = this.particles[j];
+            this.gravaite(ballA, ballB);
+        }
     }
 
-    show() {
-    	this.showMessage();
+    draw(ball) {
+        ball.draw(this.context);
+    }
+
+    drawFrmae() {
+        window.requestAnimationFrame(this.drawFrmae.bind(this), this.nodeGardenMassCanvas);
+        this.context.clearRect(0, 0, this.width, this.height);
+       
+        this.particles.forEach(this.move.bind(this));
+        this.particles.forEach(this.draw.bind(this));
     }
 }
