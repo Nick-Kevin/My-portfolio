a942b12bc5f8284391199c6d15a4d7eefe4a1e9e
Nick-Kevin <nicblou23@gmail.com>
2023-11-17 09:00:45 +0000

    update index.js

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -273,89 +273,89 @@ const router = async () => {
         case "/about":
             var nodeGardenMassCanvas = document.getElementById('node-garden-mass');
 
+            nodeGardenMassCanvas.width = window.innerWidth;
+            nodeGardenMassCanvas.height = window.innerHeight;
 
-        nodeGardenMassCanvas.width = window.innerWidth;
-        nodeGardenMassCanvas.height = window.innerHeight;
-           var context = nodeGardenMassCanvas.getContext('2d'),
-               particles = [],
-               numParticles = 50,
-               minDist = 100,
-               springAmount = 0.001;
+            var context = nodeGardenMassCanvas.getContext('2d'),
+                particles = [],
+                numParticles = 50,
+                minDist = 100,
+                springAmount = 0.001;
            
-           var width = nodeGardenMassCanvas.width;
-           var height = nodeGardenMassCanvas.height;
+            var width = nodeGardenMassCanvas.width;
+            var height = nodeGardenMassCanvas.height;
            
-           for(var i=0; i<numParticles; i++){
-               var color = Math.random()*(0xffffff);
-               var size = Math.random()*5 + 5;
-               var ball = new Ball(size, color);
+            for(var i=0; i<numParticles; i++){
+                var color = Math.random()*(0xffffff);
+                var size = Math.random()*5 + 5;
+                var ball = new Ball(size, color);
                
-                   ball.x = Math.random()*width;
-                   ball.y = Math.random()*height;
-                   ball.vx = Math.random()*6 - 3;
-                   ball.vy = Math.random()*6 - 3;
-                   ball.mass = size;
+                    ball.x = Math.random()*width;
+                    ball.y = Math.random()*height;
+                    ball.vx = Math.random()*6 - 3;
+                    ball.vy = Math.random()*6 - 3;
+                    ball.mass = size;
                
-               particles.push(ball);
-           }
+                particles.push(ball);
+            }
            
-           function gravaite(ballA, ballB){
-               var dx = ballB.x - ballA.x;
-               var dy = ballB.y - ballA.y;
-               var dist = Math.sqrt(dx*dx + dy*dy);
+            function gravaite(ballA, ballB){
+                var dx = ballB.x - ballA.x;
+                var dy = ballB.y - ballA.y;
+                var dist = Math.sqrt(dx*dx + dy*dy);
                
-               if (dist < minDist) {                   
-                context.save();
-                context.strokeStyle = "rgba(255,255,255,.3)";
-                context.beginPath();
-                context.moveTo(ballA.x, ballA.y);
-                context.lineTo(ballB.x, ballB.y);
-                context.closePath();
-                context.stroke();
-                context.restore();               
+                if (dist < minDist) {                   
+                    context.save();
+                    context.strokeStyle = "rgba(255,255,255,.3)";
+                    context.beginPath();
+                    context.moveTo(ballA.x, ballA.y);
+                    context.lineTo(ballB.x, ballB.y);
+                    context.closePath();
+                    context.stroke();
+                    context.restore();               
                 
-                var ax = dx * springAmount,
-                    ay = dy * springAmount;
-                ballA.vx += ax / ballA.mass;
-                ballA.vy += ay / ballA.mass;
-                ballB.vx -= ax / ballB.mass;
-                ballB.vy -= ay / ballB.mass;
-               }
+                    var ax = dx * springAmount,
+                        ay = dy * springAmount;
+                    ballA.vx += ax / ballA.mass;
+                    ballA.vy += ay / ballA.mass;
+                    ballB.vx -= ax / ballB.mass;
+                    ballB.vy -= ay / ballB.mass;
+                }
            }
-           function move(ballA, i){
-               ballA.x += ballA.vx;
-               ballA.y += ballA.vy;
-               if (ballA.x > nodeGardenMassCanvas.width) {
-                      ballA.x = 0;
-                    } else if (ballA.x < 0) {
-                      ballA.x = nodeGardenMassCanvas.width;
-                    }
-                    if (ballA.y > nodeGardenMassCanvas.height) {
-                      ballA.y = 0;
-                    } else if (ballA.y < 0) {
-                      ballA.y = nodeGardenMassCanvas.height;
-                    }
+
+            function move(ballA, i){
+                ballA.x += ballA.vx;
+                ballA.y += ballA.vy;
+
+                if (ballA.x > nodeGardenMassCanvas.width) {
+                    ballA.x = 0;
+                } else if (ballA.x < 0) {
+                    ballA.x = nodeGardenMassCanvas.width;
+                }
+
+                if (ballA.y > nodeGardenMassCanvas.height) {
+                    ballA.y = 0;
+                } else if (ballA.y < 0) {
+                    ballA.y = nodeGardenMassCanvas.height;
+                }
                
-               for(var ballB, j=i+1; j<numParticles; j++){
-                   ballB = particles[j];
-                   gravaite(ballA, ballB);
-               }
+                for(var ballB, j=i+1; j<numParticles; j++){
+                    ballB = particles[j];
+                    gravaite(ballA, ballB);
+                }
+            }
+           
+            function draw(ball){
+                ball.draw(context);
+            }
+           
+            (function drawFrmae(){
+                window.requestAnimationFrame(drawFrmae, nodeGardenMassCanvas);
+                context.clearRect(0, 0, width, height);
                
-           }
-           
-           //console.log(particles);
-           
-           function draw(ball){
-               ball.draw(context);
-           }
-           
-           (function drawFrmae(){
-               window.requestAnimationFrame(drawFrmae, nodeGardenMassCanvas);
-               context.clearRect(0, 0, width, height);
-               
-               particles.forEach(move);
-               particles.forEach(draw);
-           }());
+                particles.forEach(move);
+                particles.forEach(draw);
+            }());
        break;
     }
 }
