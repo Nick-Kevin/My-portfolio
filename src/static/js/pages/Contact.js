409fb6b1a9f6690c9ef365ce12dce45c62d6c349
Nick-Kevin <nicblou23@gmail.com>
2023-11-18 18:55:53 +0000

    update Contact.js

--- src/static/js/pages/Contact.js
+++ src/static/js/pages/Contact.js
@@ -51,22 +51,16 @@ export default class extends view {
 							  	<div class="box box1">
 									  <span class="icon">
 									    <svg viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    
-    <title>email [#1572]</title>
-    <desc>Created with Sketch.</desc>
-    <defs>
+										    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+										        <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -922.000000)" fill="#000">
+										            <g id="icons" transform="translate(56.000000, 160.000000)">
+										                <path d="M294,774.474 L284,765.649 L284,777 L304,777 L304,765.649 L294,774.474 Z M294.001,771.812 L284,762.981 L284,762 L304,762 L304,762.981 L294.001,771.812 Z" id="email-[#1572]">
 
-</defs>
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Dribbble-Light-Preview" transform="translate(-340.000000, -922.000000)" fill="#000">
-            <g id="icons" transform="translate(56.000000, 160.000000)">
-                <path d="M294,774.474 L284,765.649 L284,777 L304,777 L304,765.649 L294,774.474 Z M294.001,771.812 L284,762.981 L284,762 L304,762 L304,762.981 L294.001,771.812 Z" id="email-[#1572]">
-
-</path>
-            </g>
-        </g>
-    </g>
-</svg>
+										</path>
+										            </g>
+										        </g>
+										    </g>
+										</svg>
 									  </span>
 									</div>
 								</a>
