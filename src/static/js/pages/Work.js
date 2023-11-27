34c344420658f8260eb85fd081af7eae2552fe9f
Nick-Kevin <nicblou23@gmail.com>
2023-11-27 09:24:19 +0000

    update Work.js

--- src/static/js/pages/Work.js
+++ src/static/js/pages/Work.js
@@ -11,6 +11,26 @@ export default class extends view {
 		return `
 			<main id="my-work">
 				<section class="flex-display align-items-center p-relative">
+					<div id="header" class="p-absolute">
+			            <div>
+			                <h1 class="nk-text-color">About me</h1>
+			            </div>
+			            <!-------------------------- Buttons on the top right side -------------------------->
+			            <!-- Turn on/off button -->
+			            <div id="sound-icon" class="tooltip">
+			                <img id="soundOffInHome" class="other-width pointer" src="static/assets/SVG Images//sound-off-svgrepo-com.svg" alt="sounf off.svg"/>		                
+			                <img id="soundOnInHome" class="other-width pointer" src="static/assets/SVG Images/sound-on-svgrepo-com.svg" alt="sound on.svg"/>
+			                <p class="tooltiptext">
+			                	Click to turn <span id="audio-tooltip"></span> the music
+			                </p>
+			            </div>
+			            <!----------------------------------------------------------------------------------->
+			            <div class="flex-display justify-content-end align-items-center">
+			            	<div id="burger-menu">
+						      <span></span>
+						    </div>
+			            </div>	
+			        </div>
 					<h1>Front end and web development &#8595;</h1>
 				</section>
 
