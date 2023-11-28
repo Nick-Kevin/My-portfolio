41fc30de5faa8378bcffa83979325b65a679be01
Nick-Kevin <nicblou23@gmail.com>
2023-11-28 07:44:43 +0000

    update Work.js

--- src/static/js/pages/Work.js
+++ src/static/js/pages/Work.js
@@ -38,17 +38,21 @@ export default class extends view {
 				<section class="flex-display justify-content-around align-items-center">
 					<div class="work-card br-4 grid-display">
 						<img src="static/assets/Work images/bh_1.png" alt="beach hotel welcome page">
-						<div class="flex-display flex-column align-items-center justify-content-around px-3 py-3">
-							<h6>Conception d'une application web de gestion pour gérer l'école du dimanche au sein de l'église FJKM Andavoahangy Fivavahana</h6>
-							<button class="br-1">View more</button>
+						<div class="grid-display justify-content-center align-items-center px-3 py-3">
+							<h6 class="center-text">Conception d'une application web de gestion pour gérer l'école du dimanche au sein de l'église FJKM Andavoahangy Fivavahana</h6>
+							<div class="flex-display justify-content-center w-100">
+								<button class="br-1">View more</button>
+							</div>
 						</div>
 					</div>
 
 					<div class="work-card br-4 grid-display">
 						<img src="static/assets/Work images/bh_1.png" alt="beach hotel welcome page">
-						<div class="flex-display flex-column align-items-center justify-content-around px-3 py-3">
-							<h6>Beach hotel</h6>
-							<button class="br-1">View more</button>
+						<div class="grid-display justify-content-center align-items-center px-3 py-3">
+							<h6 class="center-text">Beach hotel</h6>
+							<div class="flex-display justify-content-center w-100">
+								<button class="br-1">View more</button>
+							</div>
 						</div>
 					</div>
 
