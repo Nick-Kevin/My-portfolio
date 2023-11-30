07db357bdae331587a7724e69ef13c5522cf6c03
Nick-Kevin <nicblou23@gmail.com>
2023-11-30 19:25:21 +0000

    update Work.js

--- src/static/js/pages/Work.js
+++ src/static/js/pages/Work.js
@@ -80,7 +80,7 @@ export default class extends view {
 						<div class="grid-display justify-content-center align-items-center px-3 py-3">
 							<h1 class="center-text">Sunday school</h1>
 							<div class="flex-display justify-content-center w-100">
-								<button class="bg-theme nk-text-color br-1 mt-3">View more</button>
+								<button class="bg-theme nk-text-color br-1 mt-3 view-more">View more</button>
 							</div>
 						</div>
 					</div>
