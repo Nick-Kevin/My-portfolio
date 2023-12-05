889652239e6db4c4e7afb183e28a477297bea4b8
Nick-Kevin <nicblou23@gmail.com>
2023-12-05 19:35:30 +0000

    update sunday-school.js

---
+++ src/static/js/pages/Projects/Dev/SundaySchool.js
@@ -0,0 +1,17 @@
+import view from './view.js';
+
+export default class extends view {
+	constructor() {
+		super();
+		// set the title to Home to reflect the view
+		this.setTitle("Sunday school");
+	}
+
+	async getHtml() {
+		return `
+			<main id="my-work">								
+				<h1>Sunday school</h1>
+			</main>
+		`;
+	}
+}
