d1589564c919ed088f0faaf65e4efdb372932c13
Nick-Kevin <nicblou23@gmail.com>
2023-12-06 20:40:21 +0000

    update SundaySchool.js

--- src/static/js/pages/Projects/Dev/SundaySchool.js
+++ src/static/js/pages/Projects/Dev/SundaySchool.js
@@ -12,6 +12,9 @@ export default class extends view {
 			<main id="sunday-school" class="pr-8 pl-8 py-8">
 				<a href="/work" class="none-decoration nk-text-color fa fa-arrow-left py-1 br-1 fs-2 px-4 bg-black p-fixed" data-link></a>
 				<h1 class="center-text">Sunday school</h1>
+				<div class="mt-8 image-overview">
+					<img src="static/assets/Work images/Ecole du dimanche/Page d'accueil.png" alt="Sunday school" class="w-100 br-4">
+				</div>
 			</main>
 		`;
 	}
