89fd82544caf3c790ce4c071389f072ecd2ee1f8
Nick-Kevin <nicblou23@gmail.com>
2023-12-07 20:59:31 +0000

    update SundaySchool.js

--- src/static/js/pages/Projects/Dev/SundaySchool.js
+++ src/static/js/pages/Projects/Dev/SundaySchool.js
@@ -6,4 +6,5 @@ export default new project(
     'Sunday&nbsp;school&nbsp;welcome&nbsp;page',
 	'2023',
 	'<li>React</li><li>Laravel</li><li>MySQL</li>',
+	'I undertook a transformative 6 months project at the Information Technology department of FJKM Andravoahangy Fivavahana Church in Antananarivo, Madagascar, where I developed a web application catered specifically for the Sunday School program. This locally hosted server application serves the unique needs of the church\'s Sunday School, providing a tailored solution for managing various aspects of their educational programs.'
 );
