b68f79660c7c959685da84c00d8c9b73ab6fb798
Nick-Kevin <nicblou23@gmail.com>
2023-12-07 18:40:16 +0000

    update SundaySchool.js

--- src/static/js/pages/Projects/Dev/SundaySchool.js
+++ src/static/js/pages/Projects/Dev/SundaySchool.js
@@ -7,11 +7,15 @@ export default class extends view {
 		this.setTitle("Sunday school");
 	}
 
+	projectTitle(title) {
+		return title;
+	}
+
 	async getHtml() {
 		return `
 			<main id="sunday-school" class="pr-8 pl-8 py-8">
 				<a href="/work" class="none-decoration nk-text-color fa fa-arrow-left py-1 br-1 fs-2 px-4 bg-black p-fixed" data-link></a>
-				<h1 class="center-text">Sunday school</h1>
+				<h1 class="center-text">${ this.projectTitle('Sunday School') }</h1>
 				<div class="mt-8 image-overview">
 					<img src="static/assets/Work images/Ecole du dimanche/Page d'accueil.png" alt="Sunday school" class="w-100 br-4">
 				</div>
