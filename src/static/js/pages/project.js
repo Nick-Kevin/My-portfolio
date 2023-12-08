6a97755cb54c112c12a3df79544b6e44b9fba63e
Nick-Kevin <nicblou23@gmail.com>
2023-12-08 08:33:09 +0000

    update project.js

--- src/static/js/pages/project.js
+++ src/static/js/pages/project.js
@@ -18,7 +18,7 @@ export default class {
 	async getHtml() {
 		return `
 			<main id="sunday-school" class="my-pr-8 my-pl-8 my-py-8">
-				<a href="/work" class="none-decoration nk-text-color fa fa-arrow-left my-py-1 br-1 fs-2 px-4 bg-black p-fixed" data-link></a>
+				<a href="/work" class="none-decoration nk-text-color fa fa-arrow-left my-py-1 br-1 fs-2 my-px-4 bg-black p-fixed" data-link></a>
 				<h1 class="center-text">${ this.title }</h1>
 				<div class="my-mt-8 image-overview">
 					<img src=${ this.imageOverviewSource } alt=${ this.imageOverviewAlt } class="w-100 br-4">
@@ -58,7 +58,7 @@ export default class {
 						<button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
 					</div>
 					<!-- Slider controls -->
-					<button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+					<button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
 						<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
 							<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
 								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -66,7 +66,7 @@ export default class {
 							<span class="sr-only">Previous</span>
 						</span>
 					</button>
-					<button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+					<button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-next>
 						<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
 							<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
 								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
