681626d9ceb5b45d51b8022e72257802814b72e0
Nick-Kevin <nicblou23@gmail.com>
2023-12-08 07:59:49 +0000

    udpate project.js

--- src/static/js/pages/project.js
+++ src/static/js/pages/project.js
@@ -20,10 +20,10 @@ export default class {
 			<main id="sunday-school" class="pr-8 pl-8 py-8">
 				<a href="/work" class="none-decoration nk-text-color fa fa-arrow-left py-1 br-1 fs-2 px-4 bg-black p-fixed" data-link></a>
 				<h1 class="center-text">${ this.title }</h1>
-				<div class="mt-8 image-overview">
+				<div class="my-mt-8 image-overview">
 					<img src=${ this.imageOverviewSource } alt=${ this.imageOverviewAlt } class="w-100 br-4">
 				</div>
-				<div class="mt-8 grid-display description dashed-line-top dashed-line-bottom py-4">
+				<div class="my-mt-8 grid-display description dashed-line-top dashed-line-bottom py-4">
 					<div class="grid-display">
 						<h3 class="nk-text-style">Year</h3>
 						<p> ${ this.year} </p>
