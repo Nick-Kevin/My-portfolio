5436cfe3e3b57d46c33db2a37c2b41a2c1b498fe
Nick-Kevin <nicblou23@gmail.com>
2023-12-08 08:27:07 +0000

    update SundaySchool.js

--- src/static/js/pages/Projects/Dev/SundaySchool.js
+++ src/static/js/pages/Projects/Dev/SundaySchool.js
@@ -2,7 +2,7 @@ import project from "../../project.js";
 
 export default new project(
     'Sunday school',
-    'static/assets/Work-images/Ecole-du-dimanche/Page-d-accueil.png',
+    'static/assets/Work-images/école-du-dimanche/page-d-accueil.png',
     'Sunday&nbsp;school&nbsp;welcome&nbsp;page',
 	'2023',
 	'<li>React</li><li>Laravel</li><li>MySQL</li>',
