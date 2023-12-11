59de2077f2be464de30d394e9a386f97cbe6e8c5
Nick-Kevin <nicblou23@gmail.com>
2023-12-11 09:04:22 +0000

    update project.js

--- src/static/js/pages/project.js
+++ src/static/js/pages/project.js
@@ -75,13 +75,13 @@ export default class {
 							${ this.carouselItem() }
 						</div>
 						<!-- Slider indicators -->
-						<div class="absolute ox-s w-full z-30 flex -translate-x-1/2 top-100 left-1/2 space-x-3 rtl:space-x-reverse">
+						<div class="absolute ox-s w-full pb-4 z-30 flex -translate-x-1/2 top-100 left-1/2 space-x-3 rtl:space-x-reverse">
 							<div class="carousel-indicator flex">
 								${ this.carouselIndicator() }
 							</div>
 						</div>
 						<!-- Slider controls -->
-						<button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+						<button type="button" class="absolute pt-56 top-0 start-0 z-30 flex justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
 							<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
 								<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
@@ -89,7 +89,7 @@ export default class {
 								<span class="sr-only">Previous</span>
 							</span>
 						</button>
-						<button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+						<button type="button" class="absolute top-0 end-0 z-30 flex pt-56 justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-next>
 							<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
 								<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
 									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
