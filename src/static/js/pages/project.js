7c7b3d82596ef53bc1ec79bee92a5708e6efb72a
Nick-Kevin <nicblou23@gmail.com>
2023-12-15 10:19:36 +0000

    update project.js

--- src/static/js/pages/project.js
+++ src/static/js/pages/project.js
@@ -59,51 +59,54 @@ export default class {
 
 	carousel () {		
 		let carouselSection = '';
-		const carouselLength = this.carouselItemNumber.length;		
-		const carouselItems = this.carouselsItems();
-		this.carouselsItems();
-		//console.log(carouselItems[0]);
-		const carouselIndicators = this.carouselsIndicators();
+		
+		if (this.carouselItemNumber) {
+			const carouselLength = this.carouselItemNumber.length;		
+			const carouselItems = this.carouselsItems();
+			this.carouselsItems();
+			//console.log(carouselItems[0]);
+			const carouselIndicators = this.carouselsIndicators();
 
-		for (let carouselIndex = 0; carouselIndex < carouselLength; carouselIndex++) {
-			carouselSection += `
-				<h3 class="my-mx-9 my-mt-8 nk-text-style text-end text-3xl decor">
-					${ this.carouselTitle ? this.carouselTitle[carouselIndex] : '' }
-				</h3>
-				<div class="flex justify-center ${ this.carouselTitle ? 'mt-6' : 'mt-16' }">
-					<div class="default-carousel oy-h relative overflow-x-hidden overflow-y-clip" data-carousel="slide">
-						<!-- Carousel wrapper -->
-						<div class="relative overflow-hidden br-4">
-							<!-- Items -->
-							${ carouselItems[carouselIndex] }
-						</div>
-						<!-- Slider indicators -->
-						<div class="absolute ox-s scrollbar-perso w-full pb-4 z-30 flex -translate-x-1/2 top-91 left-1/2 space-x-3 rtl:space-x-reverse">
-							<div class="carousel-indicator flex gap-x-10">
-								${ carouselIndicators[carouselIndex] }
+			for (let carouselIndex = 0; carouselIndex < carouselLength; carouselIndex++) {
+				carouselSection += `
+					<h3 class="my-mx-9 my-mt-8 nk-text-style text-end text-3xl decor">
+						${ this.carouselTitle ? this.carouselTitle[carouselIndex] : '' }
+					</h3>
+					<div class="flex justify-center ${ this.carouselTitle ? 'mt-6' : 'mt-16' }">
+						<div class="default-carousel oy-h relative overflow-x-hidden overflow-y-clip" data-carousel="slide">
+							<!-- Carousel wrapper -->
+							<div class="relative overflow-hidden br-4">
+								<!-- Items -->
+								${ carouselItems[carouselIndex] }
 							</div>
+							<!-- Slider indicators -->
+							<div class="absolute ox-s scrollbar-perso w-full pb-4 z-30 flex -translate-x-1/2 top-91 left-1/2 space-x-3 rtl:space-x-reverse">
+								<div class="carousel-indicator flex gap-x-10">
+									${ carouselIndicators[carouselIndex] }
+								</div>
+							</div>
+							<!-- Slider controls -->
+							<button type="button" class="absolute pt-56 top-0 start-0 z-30 flex justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+								<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+									<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+									</svg>
+									<span class="sr-only">Previous</span>
+								</span>
+							</button>
+							<button type="button" class="absolute top-0 end-0 z-30 flex pt-56 justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+								<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+									<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+										<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+									</svg>
+									<span class="sr-only">Next</span>
+								</span>
+							</button>
 						</div>
-						<!-- Slider controls -->
-						<button type="button" class="absolute pt-56 top-0 start-0 z-30 flex justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-							<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-								<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-								</svg>
-								<span class="sr-only">Previous</span>
-							</span>
-						</button>
-						<button type="button" class="absolute top-0 end-0 z-30 flex pt-56 justify-center h-full my-px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-							<span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-								<svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-									<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-								</svg>
-								<span class="sr-only">Next</span>
-							</span>
-						</button>
 					</div>
-				</div>
-			`;
-		}		
+				`;
+			}
+		}
 
 		return carouselSection;
 	}
