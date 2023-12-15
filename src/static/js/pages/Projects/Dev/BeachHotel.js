6f9e8f45af5f58d34470b925accb910a67dde64a
Nick-Kevin <nicblou23@gmail.com>
2023-12-15 10:06:11 +0000

    update beach hotel

--- src/static/js/pages/Projects/Dev/BeachHotel.js
+++ src/static/js/pages/Projects/Dev/BeachHotel.js
@@ -6,7 +6,7 @@ export default new project(
     'Beach hotel welcome page',
 	'2021',
 	'<li>Codeigniter</li><li>MySQL</li>',
-	'I undertook a transformative 6 months project at the Information Technology department of FJKM Andravoahangy Fivavahana Church in Antananarivo, Madagascar, where I developed a web application catered specifically for the Sunday School program. This locally hosted server application serves the unique needs of the church\'s Sunday School, providing a tailored solution for managing various aspects of their educational programs.',
+	'During my academic journey, I embarked on the development of the Beach Hotel project, an innovative hotel management application. This application streamlines the reservation process for rooms at the Beach Hotel, offering a seamless and efficient solution for guests and hotel staff alike. Through this project, I honed my skills in system design and database management, gaining valuable insights into the practical application of software development within the hospitality industry. The Beach Hotel project reflects my commitment to merging theoretical knowledge with hands-on experience, contributing to my growth as a versatile developer',
     [ 9, 4 ],
     [
         [
