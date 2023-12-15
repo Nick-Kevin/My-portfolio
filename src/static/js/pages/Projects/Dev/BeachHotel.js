28b4f0ff853cd3b198ffb65b51bff00cf2bf9b47
Nick-Kevin <nicblou23@gmail.com>
2023-12-15 09:32:08 +0000

    create beachHotel object

---
+++ src/static/js/pages/Projects/Dev/BeachHotel.js
@@ -0,0 +1,40 @@
+import project from "../../project.js";
+
+export default new project(
+    'Beach hotel',
+    'static/assets/Work-images/beach-hotel/front-office/welcome.png',
+    'Beach hotel welcome page',
+	'2021',
+	'<li>Codeigniter</li><li>MySQL</li>',
+	'I undertook a transformative 6 months project at the Information Technology department of FJKM Andravoahangy Fivavahana Church in Antananarivo, Madagascar, where I developed a web application catered specifically for the Sunday School program. This locally hosted server application serves the unique needs of the church\'s Sunday School, providing a tailored solution for managing various aspects of their educational programs.',
+    [ 9 ],
+    [
+        [
+            'static/assets/Work-images/beach-hotel/front-office/description-part1.png',
+            'static/assets/Work-images/école-du-dimanche/authentication/description-part2.png',
+            'static/assets/Work-images/beach-hotel/front-office/description-part3.png',
+            'static/assets/Work-images/beach-hotel/front-office/reservation-form-part1.png',
+            'static/assets/Work-images/beach-hotel/front-office/reservation-form-part2.png',
+            'static/assets/Work-images/beach-hotel/front-office/about-part1.png',
+            'static/assets/Work-images/beach-hotel/front-office/about-part2.png',
+            'static/assets/Work-images/beach-hotel/front-office/about-part3.png',
+            'static/assets/Work-images/beach-hotel/front-office/about-part4.png',           
+        ],
+    ],
+    [
+        [
+            'Description part 1',
+            'Description part 2',
+            'Description part 3',
+            'Reservation form',
+            'Reservation form',
+            'About part 1',
+            'About part 2',
+            'About part 3',
+            'About part 4'
+        ],
+    ],
+    [
+        'front-office',
+    ]
+);
