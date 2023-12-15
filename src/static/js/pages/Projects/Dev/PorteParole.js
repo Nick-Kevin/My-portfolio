59720b45fc9967bda53be5df56a5cd86de44e14e
Nick-Kevin <nicblou23@gmail.com>
2023-12-15 10:50:32 +0000

    create PorteParole.js

---
+++ src/static/js/pages/Projects/Dev/PorteParole.js
@@ -0,0 +1,11 @@
+import project from "../../project.js";
+
+export default new project(
+    'Porte Parole',
+    'static/assets/Work-images/porte-parole/accueil.png',
+    'Welcome page',
+	'2022',
+	'<li>Figma</li>',
+	'I proudly present my 2048 Game project, a captivating and strategic puzzle game that I developed as part of my portfolio. The 2048 Game offers players a challenging and entertaining experience, requiring tactical thinking and numerical skill. This project not only showcases my proficiency in game development but also demonstrates my commitment to creating engaging and user-friendly applications. Through the 2048 Game, I\'ve blended creativity with technical expertise, aiming to provide users with an immersive and enjoyable gaming experience.',
+    [ 'ui-ux']
+);
