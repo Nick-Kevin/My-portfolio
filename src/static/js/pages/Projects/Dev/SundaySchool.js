12c9c477a1708e41f0840712f22335401a4e73ae
Nick-Kevin <nicblou23@gmail.com>
2023-12-15 08:02:47 +0000

    update SundaySchool.js

--- src/static/js/pages/Projects/Dev/SundaySchool.js
+++ src/static/js/pages/Projects/Dev/SundaySchool.js
@@ -7,20 +7,53 @@ export default new project(
 	'2023',
 	'<li>React</li><li>Laravel</li><li>MySQL</li>',
 	'I undertook a transformative 6 months project at the Information Technology department of FJKM Andravoahangy Fivavahana Church in Antananarivo, Madagascar, where I developed a web application catered specifically for the Sunday School program. This locally hosted server application serves the unique needs of the church\'s Sunday School, providing a tailored solution for managing various aspects of their educational programs.',
-    [ 2 ],
+    [ 2, 14 ],
     [
         [
             'static/assets/Work-images/école-du-dimanche/authentication/Inscription.png',
             'static/assets/Work-images/école-du-dimanche/authentication/Connexion.png'           
         ],
+        [
+            'static/assets/Work-images/école-du-dimanche/front-office/welcome.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/class.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/search.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/student-already-exist.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/add-new-student.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/student-record-part1.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/student-record-part2.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/register.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/historics.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/bulletin.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/students.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/param-edit-account.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/account-historics.png',
+            'static/assets/Work-images/école-du-dimanche/front-office/choose-langage.png'
+        ]
     ],
     [
         [
             'Sign up',
             'Sign in'
         ],
+        [
+            'Welcome page',
+            'Monitor\'s class',
+            'Find student before record a new',
+            'Student already exist',
+            'Record new student',
+            'Student form part 1',
+            'Student form part 2',
+            'Student register',
+            'Student historic',
+            'Student Bulletin',
+            'Students list',
+            'Edit account information',
+            'Account historic',
+            'Choose langage'
+        ]
     ],
     [
         'authentication',
+        'front-office'
     ]
 );
