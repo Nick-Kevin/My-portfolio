d87eedf3a866784fd3e67b44dcab6406227e5443
Nick-Kevin <nicblou23@gmail.com>
2023-12-20 17:52:22 +0100

    update my portfolio

--- src/static/js/pages/Projects/Dev/MyPortfolio.js
+++ src/static/js/pages/Projects/Dev/MyPortfolio.js
@@ -6,7 +6,7 @@ export default new project(
     'Beach hotel welcome page',
 	'2023',
 	'<li>HTML5</li><li>CSS3</li><li>Javascript</li><li>Tailwind css</li><li>Express</li>',
-	'During my academic journey, I embarked on the development of the Beach Hotel project, an innovative hotel management application. This application streamlines the reservation process for rooms at the Beach Hotel, offering a seamless and efficient solution for guests and hotel staff alike. Through this project, I honed my skills in system design and database management, gaining valuable insights into the practical application of software development within the hospitality industry. The Beach Hotel project reflects my commitment to merging theoretical knowledge with hands-on experience, contributing to my growth as a versatile developer',
+	'My Portfolio serves as a comprehensive showcase of my professional journey, skills, and accomplishments. Designed with clarity and user-friendliness in mind, this platform offers visitors a glimpse into my expertise, projects, and aspirations. Through a blend of visual design and informative content, My portfolio encapsulates my commitment to excellence and innovation in the digital landscape.',
     [ 4 ],
     [
         [
