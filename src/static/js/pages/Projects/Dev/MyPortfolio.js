c860f2d58e781249cc65fc7e72bf4c13a5af75dc
Nick-Kevin <nicblou23@gmail.com>
2023-12-20 17:46:30 +0100

    update my portfolio

--- src/static/js/pages/Projects/Dev/MyPortfolio.js
+++ src/static/js/pages/Projects/Dev/MyPortfolio.js
@@ -1,11 +1,11 @@
 import project from "../../project.js";
 
 export default new project(
-    'Beach hotel',
+    'My portfolio',
     'static/assets/Work-images/my-portfolio/welcome.png',
     'Beach hotel welcome page',
-	'2021',
-	'<li>Codeigniter</li><li>MySQL</li>',
+	'2023',
+	'<li>HTML5</li><li>CSS3</li><li>Javascript</li><li>Tailwind css</li><li>Express</li>',
 	'During my academic journey, I embarked on the development of the Beach Hotel project, an innovative hotel management application. This application streamlines the reservation process for rooms at the Beach Hotel, offering a seamless and efficient solution for guests and hotel staff alike. Through this project, I honed my skills in system design and database management, gaining valuable insights into the practical application of software development within the hospitality industry. The Beach Hotel project reflects my commitment to merging theoretical knowledge with hands-on experience, contributing to my growth as a versatile developer',
     [ 4 ],
     [
