1f9f06561a3cf633e2adeec24d453ef579a7aa81
Nick-Kevin <nicblou23@gmail.com>
2023-12-20 17:28:59 +0100

    update beach hotel.js

--- src/static/js/pages/Projects/Dev/BeachHotel.js
+++ src/static/js/pages/Projects/Dev/BeachHotel.js
@@ -2,52 +2,26 @@ import project from "../../project.js";
 
 export default new project(
     'Beach hotel',
-    'static/assets/Work-images/beach-hotel/front-office/welcome.png',
+    'static/assets/Work-images/my-portfolio/welcome.png',
     'Beach hotel welcome page',
 	'2021',
 	'<li>Codeigniter</li><li>MySQL</li>',
 	'During my academic journey, I embarked on the development of the Beach Hotel project, an innovative hotel management application. This application streamlines the reservation process for rooms at the Beach Hotel, offering a seamless and efficient solution for guests and hotel staff alike. Through this project, I honed my skills in system design and database management, gaining valuable insights into the practical application of software development within the hospitality industry. The Beach Hotel project reflects my commitment to merging theoretical knowledge with hands-on experience, contributing to my growth as a versatile developer',
-    [ 9, 4 ],
+    [ 4 ],
     [
         [
-            'static/assets/Work-images/beach-hotel/front-office/description-part1.png',
-            'static/assets/Work-images/école-du-dimanche/authentication/description-part2.png',
-            'static/assets/Work-images/beach-hotel/front-office/description-part3.png',
-            'static/assets/Work-images/beach-hotel/front-office/reservation-form-part1.png',
-            'static/assets/Work-images/beach-hotel/front-office/reservation-form-part2.png',
-            'static/assets/Work-images/beach-hotel/front-office/about-part1.png',
-            'static/assets/Work-images/beach-hotel/front-office/about-part2.png',
-            'static/assets/Work-images/beach-hotel/front-office/about-part3.png',
-            'static/assets/Work-images/beach-hotel/front-office/about-part4.png',           
-        ],
-        [
-            'static/assets/Work-images/beach-hotel/back-office/clients-informations.png',
-            'static/assets/Work-images/beach-hotel/back-office/rooms-informations.png',
-            'static/assets/Work-images/beach-hotel/back-office/edit-client-information.png',
-            'static/assets/Work-images/beach-hotel/back-office/edit-reservation.png'
+            'static/assets/Work-images/my-portfolio/home-part-1.png',
+            'static/assets/Work-images/my-portfolio/home-part-2.png',
+            'static/assets/Work-images/my-portfolio/work.png',
+            'static/assets/Work-images/my-portfolio/about.png',           
         ]
     ],
     [
         [
-            'Description part 1',
-            'Description part 2',
-            'Description part 3',
-            'Reservation form',
-            'Reservation form',
-            'About part 1',
-            'About part 2',
-            'About part 3',
-            'About part 4'
-        ],
-        [
-            'Client table',
-            'Rooms table',
-            'Edit client information',
-            'Edit reservation'
+            'Home part 1',
+            'Home part 2',
+            'Work & project',
+            'About me'
         ]
-    ],
-    [
-        'front-office',
-        'back-office'
     ]
 );
