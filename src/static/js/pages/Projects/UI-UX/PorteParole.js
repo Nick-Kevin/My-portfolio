843e34ad9db86ee6741a150a594525d07163f013
Nick-Kevin <nicblou23@gmail.com>
2023-12-26 18:38:32 +0300

    update porte parole.js

--- src/static/js/pages/Projects/UI-UX/PorteParole.js
+++ src/static/js/pages/Projects/UI-UX/PorteParole.js
@@ -7,5 +7,8 @@ export default new project(
 	'2022',
 	'<li>Figma</li>',
 	'Presenting Porte-parole, a significant UI/UX design project I crafted during my academic journey, specifically for my UI/UX design course. Porte-parole is a meticulously designed prototype centered around the Sustainable Development Goal (SDG) of gender equality. This project showcases my skills in user interface and experience design, addressing the crucial theme of equal rights between men and women. Through thoughtful design choices and user-centric approaches, Porte-parole exemplifies my dedication to creating visually compelling and purposeful digital experiences that contribute to meaningful social causes, encapsulating the essence of UI/UX design in the realm of Sustainable Development Goals.',
-    [ 'ui-ux']
+    [ 
+        'ui-ux',
+        'static/assets/File/porte-parole.pdf'
+    ]
 );
