99f5dca046830151dd92e0c793eeb3eecac84096
Nick-Kevin <nicblou23@gmail.com>
2023-12-26 18:56:27 +0300

    last update

---
+++ src/static/js/pages/Projects/UI-UX/SundaySchoolDesign.js
@@ -0,0 +1,14 @@
+import project from "../../project.js";
+
+export default new project(
+    'Sunday School',
+    'static/assets/Work-images/école-du-dimanche/page-d-accueil.png',
+    'Sunday school',
+	'2023',
+	'<li>Figma</li>',
+	'Presenting Porte-parole, a significant UI/UX design project I crafted during my academic journey, specifically for my UI/UX design course. Porte-parole is a meticulously designed prototype centered around the Sustainable Development Goal (SDG) of gender equality. This project showcases my skills in user interface and experience design, addressing the crucial theme of equal rights between men and women. Through thoughtful design choices and user-centric approaches, Porte-parole exemplifies my dedication to creating visually compelling and purposeful digital experiences that contribute to meaningful social causes, encapsulating the essence of UI/UX design in the realm of Sustainable Development Goals.',
+    [ 
+        'ui-ux',
+        'static/assets/File/porte-parole.pdf'
+    ]
+);
